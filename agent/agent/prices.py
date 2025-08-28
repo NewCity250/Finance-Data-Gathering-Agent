3e0bf5f118c996063f1bed1d6ff7dc42776cd736
NewCity250 <garrynev10@gmail.com>
2025-08-28 16:46:13 -0400

    Delete agent/agent directory

--- agent/agent/prices.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def stooq_prices(ticker: str):
-    # Stooq uses .us for US tickers
-    url = f"https://stooq.com/q/d/l/?s={ticker.lower()}.us&i=d"
-    df = pd.read_csv(url)  # date, open, high, low, close, volume
-    return df
